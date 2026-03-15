a6134adaeae23ac34bf51e1c4a6e062f0b87e605
FluffFaceYeti <flufffaceyeti@outlook.com>
2026-03-15 15:50:36 +0000

    Updated bot status message

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,18 @@ module.exports = {
     name: "clientReady",
     once: true,
     execute(client) {
+
         console.log(`SoT_UK Bot is online as ${client.user.tag}`);
+
+        client.user.setPresence({
+            activities: [
+                {
+                    name: "Stealing your booty and taking it to Golden Sands Outpost",
+                    type: 0 // Playing
+                }
+            ],
+            status: "online"
+        });
+
     }
 };
